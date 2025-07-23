764980a9625c77c101492c6acd52676bf8169817
JovinMtl <nsanzumukizath@gmail.com>
2025-07-23 16:02:11 +0200

    added a background pattern.

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
+  <div class="muteBg">
     <!-- <NuxtRouteAnnouncer /> -->
     <!-- <NuxtWelcome /> -->
     <NuxtPage />
   </div>
 </template>
+<style scoped>
+.muteBg{
+    background-color: #f8f8f88a;
+    --bg-c: #f8f8f817;
+    --c1: #5e5c5a13;
+    background-image:  linear-gradient(var(--c1) 2px, transparent 2px), linear-gradient(90deg, var(--c1) 2px, transparent 2px), linear-gradient(var(--c1) 1px, transparent 1px), linear-gradient(90deg, var(--c1) 1px, var(--bg-c) 1px);
+    background-size: 50px 50px, 50px 50px, 10px 10px, 10px 10px;
+    background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
+    /* position: relative; */
+    /* z-index: -3; */
+    margin-top: -1rem;
+    padding-top: 1.1rem;
+    padding-top: 20px;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
     <main class="c-r fl fl-c">
         <div class="ta-c">
             Rechercher le médicament disponible dans la Pharmacie. <br>
-            <input placeholder="ex: paracetamol" /> <br>
+            <input class="m-v-5" placeholder="ex: paracetamol" /> <br>
             <button>Rechercher</button>
         </div>
     </main>
