951659d19a9e5958a70d0de7f20d0e01ebfe9ab0
JovinMtl <nsanzumukizath@gmail.com>
2025-07-23 16:30:56 +0200

    styled the UI for search.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,42 @@
 <template>
-    <main class="c-r fl fl-c">
+    <main class="fl fl-c">
         <div class="ta-c">
-            Rechercher le médicament disponible dans la Pharmacie. <br>
-            <input class="m-v-5" placeholder="ex: paracetamol" /> <br>
+            <span class="sen ">
+                <span class="color-title">
+                    Rechercher le médicament disponible dans la 
+                </span>
+                <span class="c-b">Pharmacie Ubuzima</span> .
+            </span>
+             <br>
+            <input class="m-v-5 p-5 ta-c" placeholder="ex: paracetamol" /> <br>
             <button>Rechercher</button>
         </div>
     </main>
 </template>
+
+<script setup lang="ts">
+useHead({
+  link: [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Sen:wght@400..800&display=swap'
+    }
+  ]
+})
+
+</script>
+<style scoped>
+.sen{
+    font-family: Sen;
+}
+.color-title{
+    background: linear-gradient(to right, rgb(0, 128, 107),rgb(11, 102, 11));
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+.c-b{
+    color: blue;
+}
+</style>
